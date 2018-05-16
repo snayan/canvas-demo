a19bd92623f4193ab4054ec5e37745c6526650dd
yang.zhang <yang.zhang@shopee.com>
2018-05-16 12:01:19 +0800

    postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [require('postcss-import'), require('postcss-cssnext'), require('stylelint'), require('postcss-reporter')({ clearReportedMessages: true })],
+  plugins: [
+    require('postcss-import'),
+    require('postcss-cssnext'),
+    require('stylelint'),
+    require('postcss-reporter')({ clearReportedMessages: true })
+  ],
 };
