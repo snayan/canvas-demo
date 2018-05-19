f50016ffad55182f37f54a8fe808c3403e7ae963
yang.zhang <yang.zhang@shopee.com>
2018-05-19 20:53:02 +0800

    play bgm && adjust ima

--- example/sh/index.ts
+++ example/sh/index.ts
@@ -1,7 +1,10 @@
 import CommonRender from 'common/render';
 import Canvas from 'common/canvas';
 import { isSingleModule } from 'common/util';
+import browser from 'common/browser';
 import ShCanvas from './canvas';
+
+let bgm = require('./audio/bgm.mp3');
 
 const moduleName = 'sh';
 
@@ -11,8 +14,22 @@ export default class Sh extends CommonRender {
     super(moduleName);
     this.canvas.push(new ShCanvas());
   }
+  loadBgm() {
+    let audio = document.createElement('audio');
+    document.body.appendChild(audio);
+    audio.src = bgm;
+    audio.loop = true;
+    audio.autoplay = true;
+    audio.muted = false;
+    audio.load();
+    audio.canPlayType
+    audio.onerror = (e) => {
+      console.log(e);
+    };
+    return this;
+  }
 }
 
 if (isSingleModule(moduleName)) {
-  new Sh().render();
+  new Sh().render().then((sh) => sh.loadBgm());
 }
