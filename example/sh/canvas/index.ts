5ae6153cf69b0252735b93987f6572dca71cbcc8
yang.zhang <yang.zhang@shopee.com>
2018-05-19 14:55:14 +0800

    star

--- example/sh/canvas/index.ts
+++ example/sh/canvas/index.ts
@@ -1,34 +1,26 @@
 import Canvas from 'common/canvas';
 import { FONT } from 'common/CONSTANT';
-import StarFactory from './StarFactory';
+import Star from './star';
 import styles from '../index.scss';
 
 class ShCanvas extends Canvas {
   ctx: CanvasRenderingContext2D;
   font: string;
   starCount: number;
-  starFactory: StarFactory;
+  stars: Star[];
+  meteorCount: number;
+  meteors: Star[];
   constructor() {
     super();
     this.ctx = this.getContext('2d');
-    this.starCount = 160;
-    this.starFactory = new StarFactory(this.ctx);
+    this.starCount = 280;
+    this.stars = [];
+    this.meteorCount = 20;
+    this.meteors = [];
   }
   /* 随机数 */
   private random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  private createX() {
-    return this.random(0, this.width);
-  }
-  private createY() {
-    return this.random(0, this.height);
-  }
-  private createR() {
-    return this.random(10, 50);
-  }
-  private createA() {
-    return this.random(0, 1);
   }
   /* 画背景 */
   private drawBg(ctx: CanvasRenderingContext2D) {
@@ -37,29 +29,62 @@ class ShCanvas extends Canvas {
     ctx.fillRect(0, 0, this.width, this.height);
     ctx.restore();
   }
-  /* 画星星 */
-  private drawStars(ctx: CanvasRenderingContext2D) {
-    ctx.save();
-    let { width, height, starCount, starFactory } = this;
-    for (let i = 0; i < this.starCount; i++) {
-      starFactory.create(this.createX(), this.createY(), this.createR(), this.createA());
+  /* 生成星星 */
+  private createStars() {
+    let { starCount, width, height, ctx } = this;
+    let option;
+    for (let i = 0; i < starCount; i++) {
+      option = {
+        isBlink: false,
+        isMeteor: false,
+      };
+      this.stars.push(new Star(ctx, option));
     }
-    ctx.restore();
+  }
+  /* 生成流星 */
+  private createMeteor() {
+    let { meteorCount, width, height, ctx } = this;
+    let option;
+    for (let i = 0; i < meteorCount; i++) {
+      setTimeout(() => {
+        option = {
+          isBlink: false,
+          isMeteor: true,
+        };
+        this.meteors.push(new Star(ctx, option));
+      }, i * 300);
+    }
+  }
+  /* 画星星 */
+  private drawStars() {
+    for (let star of this.stars) {
+      star.render();
+    }
+  }
+  /* 画流星 */
+  private drawMeteor() {
+    for (let star of this.meteors) {
+      star.render();
+    }
   }
   /* 开始 */
   private startSh() {
     let { width, height, ctx } = this;
     ctx.clearRect(0, 0, width, height);
-    this.drawBg(ctx);
-    this.drawStars(ctx);
+    this.drawBg(this.ctx);
+    this.drawStars();
+    this.drawMeteor();
+    requestAnimationFrame(this.startSh.bind(this));
   }
   /* 渲染 */
   public render(container: HTMLElement) {
     super.render(container, styles.sh);
     this.container.innerHTML = '';
     this.container.appendChild(this.el);
-    this.startSh();
+    this.createStars();
+    this.createMeteor();
 
+    this.startSh();
     return this;
   }
 }
---
+++ example/sh/canvas/star.ts
@@ -0,0 +1,89 @@
+/* 星星 */
+
+interface StarOption {
+  isBlink?: boolean;
+  isMeteor?: boolean;
+}
+
+class Star {
+  x: number;
+  y: number;
+  radius: number;
+  alpha: number;
+  isBlink: boolean;
+  isMeteor: boolean;
+  renderCtx: CanvasRenderingContext2D;
+  canvas: HTMLCanvasElement;
+  constructor(renderCtx: CanvasRenderingContext2D, option: StarOption) {
+    this.renderCtx = renderCtx;
+    this.isBlink = !!option.isBlink;
+    this.isMeteor = !!option.isMeteor;
+    let { x, y } = this.createPoint();
+    this.x = x;
+    this.y = y;
+    this.radius = this.isMeteor ? this.random(3, 8) : this.random(1, 3);
+    this.alpha = this.isMeteor ? 1 : this.random(0, 1);
+    this.initCanvas();
+  }
+  private initCanvas() {
+    let radius = this.radius;
+    let canvas = document.createElement('canvas');
+    canvas.width = radius * 2;
+    canvas.height = radius * 2;
+    let starCtx = canvas.getContext('2d');
+    let gradient = starCtx.createRadialGradient(radius, radius, 0, radius, radius, radius);
+    gradient.addColorStop(0.25, '#fff');
+    gradient.addColorStop(0.4, '#ccc');
+    gradient.addColorStop(0.9, 'hsl(217, 61%, 33%)');
+    gradient.addColorStop(1, 'transparent');
+    starCtx.fillStyle = gradient;
+    starCtx.beginPath();
+    starCtx.arc(radius, radius, radius, 0, Math.PI * 2);
+    starCtx.fill();
+    this.canvas = canvas;
+  }
+  /* 随机数 */
+  private random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  /* 创建坐标 */
+  private createPoint() {
+    let { renderCtx, isMeteor, radius } = this;
+    let { width, height } = renderCtx.canvas;
+    let x = this.random(isMeteor ? width / 2 : 0, width);
+    let y = isMeteor ? -radius : this.random(0, height);
+    return { x, y };
+  }
+  private blink() {
+    if (this.isBlink) {
+      if (this.alpha < 0) {
+        this.alpha += 0.01;
+      } else {
+        this.alpha -= 0.01;
+      }
+    }
+  }
+  private meteor() {
+    let { renderCtx } = this;
+    let { width, height } = renderCtx.canvas;
+    if (this.x < 0 || this.y > height) {
+      let { x, y } = this.createPoint();
+      this.x = x;
+      this.y = y;
+    }
+    this.x -= 1 * this.radius / 10;
+    this.y += 2 * this.radius / 10;
+  }
+  public render() {
+    let { renderCtx, canvas, isMeteor, isBlink } = this;
+    isBlink && this.blink();
+    isMeteor && this.meteor();
+    renderCtx.save();
+    renderCtx.globalAlpha = this.alpha;
+    renderCtx.drawImage(canvas, this.x, this.y, this.radius, this.radius);
+    renderCtx.restore();
+    return this;
+  }
+}
+
+export default Star;
