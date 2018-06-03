7f0009f0d8da2b8c5aac20017c88ba48db187ac5
snayan <snayan@sina.com>
2018-06-03 17:40:34 +0800

    main

--- example/main/index.ts
+++ example/main/index.ts
@@ -1,7 +1,9 @@
 import Clock from '../clock';
 import Sh from '../sh';
 import Axes from '../axes';
+import Cutout from '../cutout';
 
 new Clock().render();
 new Sh().render();
 new Axes().render();
+new Cutout().render();
