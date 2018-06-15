b937bfc25730622d92935618cca59dd76c9f28fa
snayan <snayan@sina.com>
2018-06-15 09:29:24 +0800

    main add zoom

--- example/main/index.ts
+++ example/main/index.ts
@@ -4,6 +4,7 @@ import Axes from '../axes';
 import Cutout from '../cutout';
 import Shape from '../shape';
 import Editor from '../editor';
+import Zoom from '../zoom';
 
 new Shape().render();
 new Clock().render();
@@ -11,3 +12,4 @@ new Sh().render();
 new Axes().render();
 new Cutout().render();
 new Editor().render();
+new Zoom().render();
